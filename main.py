528bfe3258f2388afac5a38b5d797f5530f59c34
leow00 <129173143+leow00@users.noreply.github.com>
2026-02-20 11:55:20 +0100

    madgwick and quarterion

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import time
 from sensor import sensor
+from ahrs.filters import Madgwick
 
+<<<<<<< HEAD
+
+imu_sensor = sensor(address=0x68)  # change to 0x69 if needed
+=======
 imu_sensor = sensor(address=0x68)
+>>>>>>> db6232c3ff42ffa530043a5acc99d6bd69287d68
 dt = 1/60
+<<<<<<< HEAD
+madgwick = Madgwick(sampletime=dt)
+q = np.array([1,0,0,0])
+=======
 
+>>>>>>> db6232c3ff42ffa530043a5acc99d6bd69287d68
 while True:
 
     # Read sensor data from the IMU sensor
@@ -14,11 +25,20 @@ while True:
         time.sleep(2)
         continue
 
+<<<<<<< HEAD
+    
+    accel, gyro = result
+    accel = np.array(accel)
+    gyro = np.array(gyro)
+    q = madgwick.updateIMU(q=q,gyr=gyro,acc=accel)
+=======
     # Add accelerometer and gyroscope data to the result list 
     accel, gyro = result 
+>>>>>>> db6232c3ff42ffa530043a5acc99d6bd69287d68
     print("Accelerometer data:", accel)
     print("Gyroscope data:", gyro)
     print("-" * 30)
+    print("q=", q)
 
     # Update frequency of data reading
     time.sleep(dt)
